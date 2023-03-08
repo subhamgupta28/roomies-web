aa81d19d65680550f3b89bc3b659479a83f1cdb6
= <67224573+subhamgupta28@users.noreply.github.com>
2023-03-08 23:25:09 +0530

    pages impl

--- src/compoments/Home.js
+++ src/compoments/Home.js
@@ -4,8 +4,8 @@ import {makeStyles, Paper} from "@material-ui/core";
 import PrimaryAppBar from "./AppBar";
 import Summary from "./Summary";
 import SelectRoom from "./SelectRoom";
-import AddItem from "./AddItem";
 import BottomMenu from "./BottomMenu";
+import RoomCreation from "./RoomCreation";
 
 
 const paperStyle = makeStyles((theme) => ({
@@ -20,37 +20,54 @@ export default function Home() {
     const [user, setUser] = React.useState({})
     const [rooms, setRooms] = React.useState([])
     const [currentRoom, setCurrentRoom] = React.useState(null)
+    const [userId, setUserId] = React.useState(null)
+    const [roomId, setRoomId] = React.useState(null)
 
-
-    React.useEffect(() => {
-        const fetchUuid = async () => {
-            const {data, error} = await supabase.auth.getSession()
-            const id = data.session.user.id
-            console.log(id)
-            if (id) {
-                await fetchUser(id)
-                await fetchRoom(id)
-            }
+    async function fetchUser(uuid) {
+        let {data: users, error} = await supabase
+            .from('users')
+            .select('*')
+            .eq("pk_uuid", uuid)
+        if (users) {
+            console.log("user", users[0])
+            setUserId(users[0].pk_uuid)
+            setUser(prev => prev = users[0])
         }
+    }
 
-        async function fetchUser(uuid) {
+    async function fetchRoom(userId) {
+        let {data: rooms, error} = await supabase
+            .from('rooms')
+            .select('*')
+            .eq('fk_user_id', userId)
+        setRooms(rooms)
+    }
+
+    async function fetchUuid() {
+        const {data, error} = await supabase.auth.getSession()
+        const id = data.session.user.id
+        console.log(id)
+        if (id) {
+            await fetchUser(id)
+            await fetchRoom(id)
+        }
+    }
+
+    async function fetchRoomUsers() {
+        if (userId && roomId) {
             let {data: users, error} = await supabase
                 .from('users')
                 .select('*')
-                .eq("pk_uuid", uuid)
-            console.log("user", users[0])
-            setUser(prev => prev = users[0])
+                .eq("fk_room_id", roomId)
+
         }
+    }
 
-        async function fetchRoom(userId) {
-            let {data: rooms, error} = await supabase
-                .from('rooms')
-                .select('*')
-                .eq('fk_user_id', userId)
-
-            setRooms(rooms)
-        }
-
+    React.useEffect(() => {
+        if (currentRoom)
+            setRoomId(currentRoom.pk_room_id)
+    }, [currentRoom])
+    React.useEffect(() => {
         fetchUuid().then(r => {
 
         })
@@ -66,7 +83,6 @@ export default function Home() {
             <PrimaryAppBar room={currentRoom} user={user}/>
             {rooms && <Summary room={currentRoom} user={user}/>}
             {rooms && <BottomMenu room={currentRoom} user={user}/>}
-
         </Paper>
     )
 }
--- src/compoments/SelectRoom.js
+++ src/compoments/SelectRoom.js
@@ -1,13 +1,10 @@
 import React, {useEffect} from 'react';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Paper from '@material-ui/core/Paper';
 import Draggable from 'react-draggable';
-import {Chip, TextField} from "@material-ui/core";
+import {Chip} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {Skeleton} from "@material-ui/lab";
 
